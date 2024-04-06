b4c0ae6ec62113eb867f3f65f2626592900dcacc
kalops1a <vadymduchenko@gmail.com>
2024-04-06 21:16:22 +0200

    3

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,11 @@
-export async function fetchImages(searchQuery, apiKey) {
-    const url = `https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
+import axios from 'axios';
+
+export async function fetchImages(searchQuery, apiKey, page = 1, perPage = 15) {
+    const url = `https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
 
     try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`Error: ${response.statusText}`);
-        }
-        return await response.json();
+        const response = await axios.get(url);
+        return response.data;
     } catch (error) {
         throw new Error(`An error occurred while fetching data: ${error.message}`);
     }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,24 @@ import { fetchImages } from "./js/pixabay-api";
 import { displayImages } from "./js/render-functions";
 import "izitoast/dist/css/iziToast.min.css";
 
+
+let searchQuery = '';
+let currentPage = 1;
+const apiKey = "43152327-599555690163c4dbf744e6761";
+
+document.getElementById("loadMoreBtn").addEventListener("click", async () => {
+    try {
+        const data = await fetchImages(searchQuery, apiKey, ++currentPage);
+        if (data.hits.length === 0) {
+            document.getElementById("loadMoreBtn").style.display = "none";
+        } else {
+            displayImages(data.hits);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+});
+
 const loader = document.getElementById("loader");
 
 document.getElementById("searchForm").addEventListener("submit", async evt => {
@@ -21,10 +39,10 @@ document.getElementById("searchForm").addEventListener("submit", async evt => {
         return;
     }
 
-    const apiKey = "43152327-599555690163c4dbf744e6761";
+    
 
     try {
-        const data = await fetchImages(searchInputValue, apiKey);
+        const data = await fetchImages(searchInputValue, apiKey, currentPage);
         if (data.hits.length === 0) {
             loader.style.display = "none";
             iziToast.info({
